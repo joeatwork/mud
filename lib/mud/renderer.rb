e79fdad2132dd8bf9cf8da97337b58106dc55161
Joe <joerbowers@gmail.com>
2017-04-05 21:46:48 -0700

    minor cleanup

--- lib/mud/renderer.rb
+++ lib/mud/renderer.rb
@@ -10,123 +10,21 @@ module Mud
       puts header(mud)
 
       mud.coords.each do |off_x, off_y|
-        region = SQUARE.map {|dx, dy| [off_x + dx, off_y + dy]}
-        if region.all? {|spot| mud.in_bounds(*spot) }
-          poly =
-            case region.map {|spot| mud.sample(*spot) }
-            when [true, true, true, true]
-              region
-            when [false, true, true, true]
-              [
-                between(region[0], region[1]),
-                region[1],
-                region[2],
-                region[3],
-                between(region[3], region[0]),
-              ]
-            when [true, false, true, true]
-              [
-                region[0],
-                between(region[0], region[1]),
-                between(region[1], region[2]),
-                region[2],
-                region[3]
-              ]
-            when [false, false, true, true]
-              [
-                between(region[1], region[2]),
-                region[2],
-                region[3],
-                between(region[3], region[0])
-              ]
-            when [true, true, false, true]
-              [
-                region[0],
-                region[1],
-                between(region[1], region[2]),
-                between(region[2], region[3]),
-                region[3]
-              ]
-            when [false, true, false, true]
-              [
-                between(region[0], region[1]),
-                region[1],
-                between(region[1], region[2]),
-                between(region[2], region[3]),
-                region[3],
-                between(region[3], region[0])
-              ]
-            when [true, false, false, true]
-              [
-                region[0],
-                between(region[0], region[1]),
-                between(region[2], region[3]),
-                region[3]
-              ]
-            when [false, false, false, true]
-              [
-                between(region[3], region[0]),
-                between(region[2], region[3]),
-                region[3]
-              ]
-            when [true, true, true, false]
-              [
-                region[0],
-                region[1],
-                region[2],
-                between(region[2], region[3]),
-                between(region[3], region[0])
-              ]
-            when [false, true, true, false]
-              [
-                between(region[0], region[1]),
-                region[1],
-                region[2],
-                between(region[2], region[3])
-              ]
-            when [true, false, true, false]
-              [
-                region[0],
-                between(region[0], region[1]),
-                between(region[1], region[2]),
-                region[2],
-                between(region[2], region[3]),
-                between(region[3], region[0])
-              ]
-            when [false, false, true, false]
-              [
-                between(region[1], region[2]),
-                region[2],
-                between(region[2], region[3])
-              ]
-            when [true, true, false, false]
-              [
-                region[0],
-                region[1],
-                between(region[1], region[2]),
-                between(region[3], region[0])
-              ]
-            when [false, true, false, false]
-              [
-                between(region[0], region[1]),
-                region[1],
-                between(region[1], region[2])
-              ]
-            when [true, false, false, false]
-              [
-                region[0],
-                between(region[0], region[1]),
-                between(region[3], region[0])
-              ]
-            when [false, false, false, false]
-              []
-            end
+        region = SQUARE.map { |dx, dy| [off_x + dx, off_y + dy] }
+        next unless region.all? { |spot| mud.in_bounds(*spot) }
 
-          unless poly.empty?
-            pts_expr = poly.map {|x, y| "#{x * @scale} #{y * @scale}"}.join(', ')
-            puts %(  <polygon points="#{pts_expr}" fill="black" />)
-          end
+        samples = region.map { |spot| mud.sample(*spot) }
+        poly = march_square(samples)
+        next if poly.empty?
+
+        pts = poly.map do |poly_x, poly_y|
+          x = (off_x + poly_x) * @scale
+          y = (off_y + poly_y) * @scale
+          "#{x} #{y}"
         end
+
+        pts_expr = pts.join(', ')
+        puts %(  <polygon points="#{pts_expr}" fill="black" />)
       end
 
       puts '  <g display="none">'
@@ -166,6 +64,117 @@ module Mud
       dx = (x2 - x1) / 2.0
       dy = (y2 - y1) / 2.0
       [x1 + dx, y1 + dy]
+    end
+
+    def march_square(samples)
+      case samples
+      when [true, true, true, true]
+        SQUARE
+      when [false, true, true, true]
+        [
+          between(SQUARE[0], SQUARE[1]),
+          SQUARE[1],
+          SQUARE[2],
+          SQUARE[3],
+          between(SQUARE[3], SQUARE[0])
+        ]
+      when [true, false, true, true]
+        [
+          SQUARE[0],
+          between(SQUARE[0], SQUARE[1]),
+          between(SQUARE[1], SQUARE[2]),
+          SQUARE[2],
+          SQUARE[3]
+        ]
+      when [false, false, true, true]
+        [
+          between(SQUARE[1], SQUARE[2]),
+          SQUARE[2],
+          SQUARE[3],
+          between(SQUARE[3], SQUARE[0])
+        ]
+      when [true, true, false, true]
+        [
+          SQUARE[0],
+          SQUARE[1],
+          between(SQUARE[1], SQUARE[2]),
+          between(SQUARE[2], SQUARE[3]),
+          SQUARE[3]
+        ]
+      when [false, true, false, true]
+        [
+          between(SQUARE[0], SQUARE[1]),
+          SQUARE[1],
+          between(SQUARE[1], SQUARE[2]),
+          between(SQUARE[2], SQUARE[3]),
+          SQUARE[3],
+          between(SQUARE[3], SQUARE[0])
+        ]
+      when [true, false, false, true]
+        [
+          SQUARE[0],
+          between(SQUARE[0], SQUARE[1]),
+          between(SQUARE[2], SQUARE[3]),
+          SQUARE[3]
+        ]
+      when [false, false, false, true]
+        [
+          between(SQUARE[3], SQUARE[0]),
+          between(SQUARE[2], SQUARE[3]),
+          SQUARE[3]
+        ]
+      when [true, true, true, false]
+        [
+          SQUARE[0],
+          SQUARE[1],
+          SQUARE[2],
+          between(SQUARE[2], SQUARE[3]),
+          between(SQUARE[3], SQUARE[0])
+        ]
+      when [false, true, true, false]
+        [
+          between(SQUARE[0], SQUARE[1]),
+          SQUARE[1],
+          SQUARE[2],
+          between(SQUARE[2], SQUARE[3])
+        ]
+      when [true, false, true, false]
+        [
+          SQUARE[0],
+          between(SQUARE[0], SQUARE[1]),
+          between(SQUARE[1], SQUARE[2]),
+          SQUARE[2],
+          between(SQUARE[2], SQUARE[3]),
+          between(SQUARE[3], SQUARE[0])
+        ]
+      when [false, false, true, false]
+        [
+          between(SQUARE[1], SQUARE[2]),
+          SQUARE[2],
+          between(SQUARE[2], SQUARE[3])
+        ]
+      when [true, true, false, false]
+        [
+          SQUARE[0],
+          SQUARE[1],
+          between(SQUARE[1], SQUARE[2]),
+          between(SQUARE[3], SQUARE[0])
+        ]
+      when [false, true, false, false]
+        [
+          between(SQUARE[0], SQUARE[1]),
+          SQUARE[1],
+          between(SQUARE[1], SQUARE[2])
+        ]
+      when [true, false, false, false]
+        [
+          SQUARE[0],
+          between(SQUARE[0], SQUARE[1]),
+          between(SQUARE[3], SQUARE[0])
+        ]
+      when [false, false, false, false]
+        []
+      end
     end
   end
 end
