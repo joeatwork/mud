9419edb003881c76268935b6cefc0f11fddb4a8e
Joe <joe@triplebyte.com>
2017-04-25 13:22:56 -0700

    simple html for development

--- js/webpack.config.js
+++ js/webpack.config.js
@@ -1,4 +1,4 @@
-/* global module */
+/* global module require __dirname */
 // Cargo culted off of the internet.
 
 var path = require('path');
@@ -6,7 +6,7 @@ var path = require('path');
 module.exports = {
   entry: path.resolve(__dirname, 'src/index.jsx'),
   output: {
-    path: path.resolve(__dirname, 'public'),
+    path: path.resolve(__dirname, 'public/build'),
     filename: 'bundle.js',
   },
    module : {
