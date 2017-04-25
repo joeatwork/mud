844994ba112fb85717571bfc6f38dadaadba7577
Joe <joe@triplebyte.com>
2017-04-25 14:53:37 -0700

    skeleton react component

--- js/src/index.jsx
+++ js/src/index.jsx
@@ -1,2 +1,10 @@
-console.log("It's alive!");
+
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const Root = () => {
+  return <h1>IS THIS THING ON?</h1>;
+};
+
+ReactDom.render(<Root />, document.getElementById('root'));
 
