ee2c89de7f92d4f74be923f5b3d48f5714bd765b
Joe <joerbowers@gmail.com>
2017-05-20 16:22:02 -0700

    Remove ruby stuff!

--- lib/mud/version.rb
+++
@@ -1,3 +0,0 @@
-module Mud
-  VERSION = "0.1.0"
-end
--- spec/mud_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-
-RSpec.describe Mud do
-
-  it 'has a version number' do
-    expect(Mud::VERSION).not_to be nil
-  end
-end
